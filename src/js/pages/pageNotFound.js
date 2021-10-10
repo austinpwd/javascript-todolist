cc38dd79396565ce7cf3dc112dd048260ad47d2d
Austin D <doan.austin.pw@gmail.com>
2021-10-10 05:31:55 -0600

    Fixed a typo

--- src/js/pages/pageNotFound.js
+++ src/js/pages/pageNotFound.js
@@ -1,15 +1,14 @@
 import brandingHeader from '../components/brandingHeader'
-import makeElement from '../utils/makeElement'
 import link from '../components/ui/link'
 
 const pageNotFound = function(){
 
     const pageHeader = document.createElement('header')
     pageHeader.appendChild((brandingHeader()))
-    pageHeader.appendChild(link("Go to To-Do List app", "/todo", ))
+    pageHeader.appendChild(link("Go to To-Do List app", "/pageNotFound", ))
 
     return pageHeader
       
  }
  
- export default pageNotFoumd
+ export default pageNotFound
